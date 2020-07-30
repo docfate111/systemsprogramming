60b770a7722e478a2f0b4fc277204565926cbb9e
docfate111 <tdwilliamsiv@gmail.com>
2020-07-30 17:33:45 -0600

    implemented threading

--- networking/getrequest.c
+++ networking/getrequest.c
@@ -8,7 +8,15 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <netdb.h>
-int get(const char* ip_addr, int port_num, const char* data){
+typedef struct{
+	char *address;
+	int port_num;
+} lump;
+void* get(void* arg){
+	lump* argh = arg;
+	char* ip_addr = argh->address;
+	int port_num = argh->port_num;
+	const char* data =  "";
 	int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if ( socket_desc == -1){
 		printf("Could not create socket");
@@ -20,7 +28,7 @@ int get(const char* ip_addr, int port_num, const char* data){
 	//Connect to remote server
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
 		puts("connect error");
-		return 1;
+		return NULL;
 	}
 	puts("Connected");
 	char* message = (char*)malloc(60);
@@ -28,7 +36,7 @@ int get(const char* ip_addr, int port_num, const char* data){
 	if( send(socket_desc , message , strlen(message) , 0) < 0)
 	{
 		puts("Request failed");
-		return 1;
+		return NULL;
 	}
 	puts(message);
 	puts("Request Sent\n");
@@ -39,10 +47,11 @@ int get(const char* ip_addr, int port_num, const char* data){
 	puts("Response received!");
 	puts(server_reply);
 	close(socket_desc);
-	return 0;
+	return NULL;
 }
+
 char* convert_hostname(char* hostname){
-	char* ip = (char*)malloc(14);
+	char* ip = (char*)malloc(20);
 	struct hostent *he;
 	struct in_addr **addr_list;
 	int i;
@@ -66,23 +75,27 @@ int isIp(char* b){
 			count++;
 		}
 	}
-	return count==4;
+	return count==3;
 }
 int main(int argc, char* argv[]){
 	if(argc<3){
 		printf("Usage <%s>: <url/ip address> <port>", argv[0]);
 		return 0;
 	}
+	lump* l = (lump*)malloc(sizeof(lump));
 	char *url = (char*)malloc(12);
 	strncpy(url, argv[1], 12);
 	char* p = (char*)malloc(4);
 	strncpy(p, argv[2], 4);
-	char* hostname;
+	l->address = (char*)malloc(20);
 	if(!isIp(url)){
-		hostname = convert_hostname(url);
+		l->address = convert_hostname(url);
 	}else{
-		hostname = url;
+		l->address = url;
 	}
-	get(hostname, atoi(p), "");
+	l->port_num = atoi(p);
+	pthread_t th;
+	int status = pthread_create(&th, NULL, get, (void*)l);
+	pthread_join(th, NULL);
 	return 0;
 }
