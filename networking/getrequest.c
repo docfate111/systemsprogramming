158d96744cfa1008669a1406b7c0d09d19989f4b
docfate111 <tdwilliamsiv@gmail.com>
2020-07-30 16:00:47 -0600

    finished

--- networking/getrequest.c
+++ networking/getrequest.c
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <netdb.h>
 int get(const char* ip_addr, int port_num, const char* data){
 	int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if ( socket_desc == -1){
@@ -40,8 +41,48 @@ int get(const char* ip_addr, int port_num, const char* data){
 	close(socket_desc);
 	return 0;
 }
-
-int main(int argc, char** argv){
-	get("127.0.0.1", 8000, "file.html");
+char* convert_hostname(char* hostname){
+	char* ip = (char*)malloc(14);
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+	if ( (he = gethostbyname( hostname ) ) == NULL){
+		//gethostbyname failed
+		herror("gethostbyname");
+		return NULL;
+	}
+	//Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
+	addr_list = (struct in_addr **) he->h_addr_list;
+	for(i = 0; addr_list[i] != NULL; i++){
+		//Return the first one;
+		strcpy(ip , inet_ntoa(*addr_list[i]) );
+	}
+	return ip;
+}
+int isIp(char* b){
+	int count = 0;
+	for(size_t i=0; i<strlen(b); i++){
+		if(*(b+i)=='.'){
+			count++;
+		}
+	}
+	return count==4;
+}
+int main(int argc, char* argv[]){
+	if(argc<3){
+		printf("Usage <%s>: <url/ip address> <port>", argv[0]);
+		return 0;
+	}
+	char *url = (char*)malloc(12);
+	strncpy(url, argv[1], 12);
+	char* p = (char*)malloc(4);
+	strncpy(p, argv[2], 4);
+	char* hostname;
+	if(!isIp(url)){
+		hostname = convert_hostname(url);
+	}else{
+		hostname = url;
+	}
+	get(hostname, atoi(p), "");
 	return 0;
 }
