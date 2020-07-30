a219185b0182e1b4a5ef84fa33bc1ee15212eb01
docfate111 <tdwilliamsiv@gmail.com>
2020-07-30 12:37:23 -0600

    threads and needles

--- threads/thread1.c
+++ threads/thread1.c
@@ -4,12 +4,22 @@
 void* myturn(void* arg){
 	for(int i=0; i<8; i++){
 		sleep(1);
-		printf("My turn %d", i);
+		printf("My turn %d\n", i);
 	}
+	return NULL;
 }
 void yourturn(){
 	for(int i=0; i<3; i++){
-	
+		sleep(1);
+		printf("Your turn %d\n", i);
+	}
+}
 int main(){
+	pthread_t newthread;
+	pthread_create(&newthread, NULL, myturn, NULL);
+	yourturn();
+	//wait until newthread completes
+	pthread_join(newthread, NULL);
+	puts("thread's done!");
 	return 0;
 }
